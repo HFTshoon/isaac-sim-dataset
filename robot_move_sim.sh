b0522beb6739df95211dfdcfdb43864f4db76853
HFTshoon <shoon0602@snu.ac.kr>
2026-05-09 20:58:10 +0900

    add dataset for robot move

---
+++ robot_move_sim.sh
@@ -0,0 +1,5 @@
+./python.sh corl2025/scenes/01_base_robot_move.py --scene-json corl2025/dataset/01_robot/scene_000/isaac_objects_for_moveit.json
+./python.sh corl2025/scenes/01_base_robot_move.py --scene-json corl2025/dataset/01_robot/scene_001/isaac_objects_for_moveit.json
+./python.sh corl2025/scenes/01_base_robot_move.py --scene-json corl2025/dataset/01_robot/scene_002/isaac_objects_for_moveit.json
+./python.sh corl2025/scenes/01_base_robot_move.py --scene-json corl2025/dataset/01_robot/scene_003/isaac_objects_for_moveit.json
+./python.sh corl2025/scenes/01_base_robot_move.py --scene-json corl2025/dataset/01_robot/scene_004/isaac_objects_for_moveit.json
