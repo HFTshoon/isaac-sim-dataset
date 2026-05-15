6953aada48ca3fde4d02d2a7b6900698aa263312
HFTshoon <shoon0602@snu.ac.kr>
2026-05-15 17:02:17 +0900

    add rrt exp with moveit (not finished)

--- robot_experiment_curobo.sh
+++ robot_experiment_curobo.sh
@@ -3,46 +3,46 @@
 # scene02 0 1 5 7 8
 
 # curobo
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 000 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 000 --headless --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 001 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 001 --headless --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 002 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 002 --headless --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 003 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 003 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 000 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 000 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 001 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 001 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 002 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 002 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 003 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 003 --headless --include-target
 ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 005 --headless
 ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 005 --headless --include-target
  
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 000 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 000 --headless --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 001 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 001 --headless --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 005 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 005 --headless --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 007 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 007 --headless --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 008 --headless
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 008 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 000 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 000 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 001 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 001 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 005 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 005 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 007 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 007 --headless --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 008 --headless
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 008 --headless --include-target
 
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 000 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 000 --headless --jitter --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 001 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 001 --headless --jitter --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 002 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 002 --headless --jitter --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 003 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 003 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 000 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 000 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 001 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 001 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 002 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 002 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 003 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 003 --headless --jitter --include-target
 ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 005 --headless --jitter
 ./python.sh corl2025/scenes/experiment_curobo.py --scene 01 --scene-num 005 --headless --jitter --include-target
  
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 000 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 000 --headless --jitter --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 001 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 001 --headless --jitter --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 005 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 005 --headless --jitter --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 007 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 007 --headless --jitter --include-target
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 008 --headless --jitter
-# ./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 008 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 000 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 000 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 001 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 001 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 005 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 005 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 007 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 007 --headless --jitter --include-target
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 008 --headless --jitter
+./python.sh corl2025/scenes/experiment_curobo.py --scene 02 --scene-num 008 --headless --jitter --include-target
